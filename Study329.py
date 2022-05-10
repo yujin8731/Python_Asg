7fc6f060bb7ae3dce78ed223f9e60334a93108fc
yujin8731 <yujin8731@daum.net>
2022-05-10 13:21:11 +0900

    파일 추가

---
+++ Study329.py
@@ -0,0 +1,204 @@
+# # # # data=int(input("Enter a numver:"))
+# # # # print(f"Received: {data+10}")
+# # #
+# # #
+# # #
+# # #
+# # #
+# # # #
+# # # #
+# # # # data= int((input("enter a number")))
+# # # # print(f"Received:")
+# # # #
+# # # # # # # somebody=input("enver a number")
+# # # # # # # # print("hi", somebody)
+# # # # age=int(input("How old r u?"))
+# # # # print("age=", age+10)
+# # # # # #
+# # # # # # # x=10.9890
+# # # # # # # print(x)
+# # # # # # # x= int(x)
+# # # # # # # print(x)
+# # # # # # #
+# # # # # # # x=10.87
+# # # # # # # print(type(x))
+# # # # # # # x=int(x)
+# # # # # # # print(type(x))
+# # # # # # #
+# # # # # # # x="hello"*7
+# # # # # # # print(len(x))
+# # # # # #
+# # # # # #
+# # # print("Good\n"+"morning")
+# # # print("Good\t morning")
+# # # # # #
+# # # # # # print("\\hello")
+# # # # # #
+# # # # # # print("Hello ")
+# # # # #
+# # # # #
+# # # # # data=10
+# # # # # print("%x" %data) #8진수 출력
+# # # # #
+# # # #
+# # # # print(len("안녕하세요"))
+# # # # # data=17
+# # # # # print("%x", %data)
+# # # # # # #2진수 출력
+# # # # # # data=bin(10)
+# # # # # # print(data)
+# # # # #
+# # # # # pin=3.14
+# # # # # x=3
+# # # # # y=4
+# # # #
+# # # # print("I have %d apples" %5)
+# # # # print("Error is %F%%" %3.23)
+# # # # # print("%o %x %X" %(109,34,7333))
+# # # #
+# # # # print("I have {} apples.".format("five"))
+# # # # print("I have {} apples.".format("5"))
+# # # #
+# # # #
+# # # somebody= int(input("how old are you?:"))
+# # # print("hi I am", somebody)
+# #
+# # # string1=str(123)
+# # # string2=str(546)
+# # # print("string1+ string2:", string1+string2)
+# # #
+# # # number1=int("123")
+# # # number2=int("456")
+# # # print("number1+number2", number1+number2)
+# #
+# # a=int(input("a:"))
+# # b=int(input("b:"))
+# # print("a+b:", a+b)
+# #
+# # #
+# # # # # print("X=",x)
+# # # # # print("x=%d","Y=%d" %(x,Y))
+# # # #
+# # # # # pin=3.14
+# # # # # x=3
+# # # # # y=4
+# # # # # # print("X=",x)
+# # # # # # print("hello,%s" %input())
+# # # #
+# # # # # print("Hello,%s"%"five")
+# # # # # print("hello,%d" %30)
+# # # # # print("hello,%f%%" %30.123)
+# # # #
+# # # # #프린트 13
+# # # # #
+# # # # # pin=3.14
+# # # # # x=3
+# # # # # y=4
+# # # #
+# # # # # str_01="{}".format("five")
+# # # # # data="five"
+# # # # # str_01=f"{data}"
+# # # # # print(str_01)
+# # # # # print("{}".format("five"))
+# # # # # print("{}".format("A"))
+# # # # # print("{}".format(3))
+# # # # # print("{}%".format(3.87))
+# # # # # print("{:f}%".format(3))
+# # # # # print("{x}{y}{z}".format(x=3,y=10,z=30))
+#
+#
+#
+# A= int(input("A:"))
+# B= int(input("B:"))
+#
+#
+# print(f"A+B: {A+B}")
+#
+#
+#
+# # # #
+# # # # # pin=3.14
+# # # # x=3
+# # # # # y=4
+# # # # # print(f"{x}")
+# # # #
+# # # # #인덱스 출력
+# # # # # data="Hello"
+# # # # # print(len(data))
+# # # # # print(data)
+# # # # # print(data[0])
+# # # # # print(data[-1])
+# # # # #
+# # # # # print(data[0:2])
+# # # # # print(data[1:3])
+# # # # # print(data[:3])
+# # # # # print(data[::2])
+# # # #
+# # a=int(input("First: "))
+# # b=int(input("second: "))
+# # print(f"A+B={a+b}")
+# # print(f"A-B={a-b}")
+# # print(f"A*B={a*b}")
+# # # print(f"A/B={a/b}")
+# # # #
+# # # # # from urllib.request import urlopen
+# # # # #
+# # # # # f=urlopen("https://www.python.org")
+# # # # # print(f.headers)
+# # # #
+# # # # #과제
+# # # #
+# # # # import random
+# # # #
+# # # # data=random.randint(1,6)
+# # # # i=1
+# # # # print("Random #%d: %d" %(i,random.randint(1,6)))
+# # # # i=2
+# # # # print("Random #%d: %d" %(i,random.randint(1,6)))
+# # # # i=3
+# # # # print("Random #%d: %d" %(i,random.randint(1,6)))
+# # # # i=4
+# # # # print("Random #%d: %d" %(i,random.randint(1,6)))
+# # # #
+# # # # ""
+# # # # # #과제- 로또 비복원, 다시 선택되면 안됨
+# # # # # [1-45]5개임의의 숫자
+# # # # # 12345
+# # # # # 11345x 중복해서 나옴 이전에 가지고 있던 수 가지고있어야 함 따라서 랜덤으로 숫자 뽑음
+# # # # #랜덤 함수를 뽑는데 내가 이미 뽑은 수 어딘가에 저장해놔야 함 string사용
+# # # # # string
+# # # # # buffer="0102"
+# # # # # r=2
+# # # # # print(buffer[0:2])
+# # # # # print(int(buffer[0:2]))
+# # # #
+# # # # # string가지고 while문 사용해
+# # # # #
+# # # # #
+# # # # # 최종적으로 12345같이 나와야 함
+# # # #
+# # # #
+# # # # buffer="0102"
+# # # # r=2  #2넣으면 나오고 4넣으면 안나옴
+# # # # r= "0"+str(r)  #02
+# # # #
+# # # # if r in buffer: #if 10 buffer안됨됨
+# # # #    print("True")
+# # # # else:
+# # # #     print("False")
+# # # #
+# # # #
+# # # # # 01
+# # # # # 02
+# # # # # 03
+# # # # # 04 이런식으로 출력
+# # # #
+# # # #
+# # # # # 1:"1"
+# # # # # 2:"12"
+# # # # # 3:"123"
+# # # #
+# # # #
+import random
+i=1
+print("random is #%d: %d" %(i,random.randint(1,4)))
